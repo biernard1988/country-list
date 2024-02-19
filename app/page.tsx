bd68ce2fb05cf3a97b122847b195932f4ec81970
Bernard Almeida da Costa <biernard@gmail.com>
2024-02-19 16:08:45 +0100

    added a input search to filter the countries by typing the name

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import CountryCard from "./components/country-card";
 
-export type Country = {
+interface Country {
   name: {
     common: string;
     official: string;
@@ -9,35 +11,68 @@ export type Country = {
     svg: string;
     alt: string;
   };
-  capital: string;
-  region: string;
-  subregion: string;
-  population: number;
-  languages?: {
-    [key: string]: string;
-  };
-  borders?: string[];
-  cca3: string;
-};
+}
 
 async function getCountries(): Promise<Country[]> {
   const response = await fetch("https://restcountries.com/v3.1/all");
   return response.json();
 }
 
-export default async function Home() {
-  const countries = await getCountries();
-  console.log(countries);
-  return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2 mt-10">
-      {countries.map((country) => (
-        <CountryCard
-          key={country.name.common}
-          name={country.name.common}
-          flag={country.flags.svg}
-          flagAlt={country.flags.alt}
-        />
-      ))}
-    </section>
+const Home: React.FC = () => {
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(
+    null
   );
-}
+
+  useEffect(() => {
+    async function fetchCountries() {
+      const fetchedCountries = await getCountries();
+      setCountries(fetchedCountries);
+    }
+    fetchCountries();
+  }, []);
+
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const searchText = event.target.value;
+    setSearchTerm(searchText);
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+    setTypingTimeout(
+      setTimeout(() => {
+        setSearchTerm(searchText);
+      }, 300)
+    );
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="flex flex-col items-center w-full">
+      <input
+        className="text-black p-3 my-3 outline-0 rounded-lg"
+        type="search"
+        placeholder="Search countries..."
+        value={searchTerm}
+        onChange={handleSearchTermChange}
+      />
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 container gap-2 mt-10">
+        {filteredCountries.map((country, index) => (
+          <CountryCard
+            key={index}
+            name={country.name.common}
+            flag={country.flags.svg}
+            flagAlt={country.flags.alt}
+          />
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default Home;
